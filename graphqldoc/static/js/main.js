70bd7e7e8191659cfd124037d538314a80666e86
Robson Cardoso dos Santos <cardoso.rcs@gmail.com>
2019-02-19 01:03:51 -0300

    ADD search bar

--- graphqldoc/static/js/main.js
+++ graphqldoc/static/js/main.js
@@ -2,4 +2,14 @@ $( document ).ready(function() {
   $(".dropdown-trigger").dropdown();
   $('.sidenav').sidenav();
   $('.collapsible').collapsible();
+
+  $("#search").on("keyup", function(e) {
+    if (e.which == 27) {  // catch ESC key and clear input
+      $(this).val('');
+    }
+    var value = $(this).val().toLowerCase();
+    $("#menu-fields li").filter(function() {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+    });
+  });
 });
